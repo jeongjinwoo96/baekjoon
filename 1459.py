bd51e7e38e7b6b501b6aef2f3ee6d4965f866d16
JINWOO JEONG <62453929+jeongjinwoo96@users.noreply.github.com>
2021-09-09 18:52:20 +0900

    walk_Greedy

--- 1459.py
+++ 1459.py
@@ -12,15 +12,18 @@ if x==0 or y==0:
             print((M-1)*s + w)
         else:
             print(M*w)
+
 elif w*2 < s:
     print((x+y)*w)
-elif (x+y - 2*min(x,y)) % 2 == 0:
+    
+elif (x+y - 2 * m) % 2 == 0:
     if w<s:
-        print(min(x,y)*s + (x+y-2*min(x,y))*w)
+        print(m * s + (x+y-2 * m) * w)
     else:
-        print(min(x,y)*s + (x+y-2*min(x,y))*s)
+        print(m * s + (x+y-2 * m) * s)
+        
 else:
-    if (x+y-2*min(x,y)-1)*s + w < (x+y-2*min(x,y))*w:
-        print(min(x,y)*s + (x+y-2*min(x,y)-1)*s + w)
+    if s < w:
+        print(m*s + (x+y-2*m-1)*s + w)
     else:
-        print(min(x,y)*s + (x+y-2*min(x,y))*w)
+        print(m*s + (x+y-2*m)*w)
